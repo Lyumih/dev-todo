63299e47dc9e1676a08bd611a30bdea02fcd3e90
midevelop <mi@elbrusboot.camp>
2019-11-01 22:22:21 +0300

    feat(web): add tree in js

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,71 @@
-let app = new Vue({
-  el: '#app',
+// demo data
+var treeData = {
+  name: 'Разработка',
+  children: [
+    { name: 'Создание макета' },
+    { name: 'Создание репозитория' },
+    {
+      name: 'Настройка проекта',
+      children: [
+        {
+          name: 'Инициировать npm'
+        },
+        {
+          name: 'Подключить пакеты',
+          children: [{ name: 'eslint' }, { name: 'react' }]
+        },
+        { name: 'Тестовый запуск' }
+      ]
+    }
+  ]
+};
+
+// define the tree-item component
+Vue.component('tree-item', {
+  template: '#item-template',
+  props: {
+    item: Object
+  },
+  data: function() {
+    return {
+      isOpen: false
+    };
+  },
+  computed: {
+    isFolder: function() {
+      return this.item.children && this.item.children.length;
+    }
+  },
+  methods: {
+    toggle: function() {
+      if (this.isFolder) {
+        this.isOpen = !this.isOpen;
+      }
+    },
+    makeFolder: function() {
+      if (!this.isFolder) {
+        this.$emit('make-folder', this.item);
+        this.isOpen = true;
+      }
+    }
+  }
+});
+
+// boot up the demo
+var demo = new Vue({
+  el: '#demo',
   data: {
-    todos: [
-      'Todo 1',
-      'Todo 2',
-      ['Todo 2-1', 'Todo 2-2', ['Todo 2-2-1', 'Todo 2-2-2']],
-      'Todo 3-1'
-    ]
+    treeData: treeData
+  },
+  methods: {
+    makeFolder: function(item) {
+      Vue.set(item, 'children', []);
+      this.addItem(item);
+    },
+    addItem: function(item) {
+      item.children.push({
+        name: 'new stuff'
+      });
+    }
   }
 });
